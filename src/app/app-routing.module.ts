64e4830091f96e4158baa1ee745b526081ebe9ec
Maciej Paluch <maciekpal@gmail.com>
2023-03-02 20:22:30 +0100

    Feat: component sign-in added to route /sign-in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignInComponentModule } from './components/sign-in/sign-in.component-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'sign-in', component: SignInComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), SignInComponentModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
