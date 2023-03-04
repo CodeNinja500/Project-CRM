544b366b4a2e24c8129ec15aaa7b9c8e3c357216
Maciej Paluch <maciekpal@gmail.com>
2023-03-04 14:15:54 +0100

    Feat: Verify guards body added

--- src/app/guards/verifed.guard.ts
+++ src/app/guards/verifed.guard.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable, take } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { map, Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VerifedGuard implements CanActivate {
-  constructor(private _authService: AuthService) {}
+  constructor(private _authService: AuthService, private _router: Router) {}
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    this._authService.me().pipe(take(1)).subscribe();
-    return true;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
+    return this._authService.me().pipe(
+      map((response) => {
+        console.log(response.email_verified);
+        return response.email_verified === true ? true : this._router.parseUrl('/verify');
+      })
+    );
   }
 }
