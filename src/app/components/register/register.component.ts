3b60439f4bd024887cdd31fd5c0f222fb42dd1ad
Maciej Paluch <maciekpal@gmail.com>
2023-03-04 12:28:16 +0100

    Feat: onRegisterMorSubmitted function created

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,27 +1,30 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { take } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 export const hasSmallLetter: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
   const password = control.value as string;
-  if (password.match(/[a-z]+/)) return null;
+  if (password.match(/[a-z]+/) || !password) return null;
   else return { hasSmallLetter: true };
 };
 
 export const hasCapitalLetter: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
   const password = control.value as string;
-  if (password.match(/[A-Z]+/)) return null;
+  if (password.match(/[A-Z]+/) || !password) return null;
   else return { hasCapitalLetter: true };
 };
 
 export const hasSpecialSign: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
   const password = control.value as string;
-  if (password.match(/[^a-zA-Z]+/)) return null;
+  if (password.match(/[^a-zA-Z0-9]+/) || !password) return null;
   else return { hasSpecialSign: true };
 };
 
 export const hasNumber: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
   const password = control.value as string;
-  if (password.match(/[0-9]+/)) return null;
+  if (password.match(/[0-9]+/) || !password) return null;
   else return { hasNumber: true };
 };
 
@@ -59,4 +62,22 @@ export class RegisterComponent {
     },
     { validators: passwordsMatch }
   );
+
+  constructor(private _authService: AuthService, private _router: Router) {}
+
+  onRegisterFormSubmitted(registerForm: FormGroup): void {
+    if (registerForm.valid) {
+      this._authService
+        .register({
+          email: registerForm.get('email')?.value,
+          password: registerForm.get('password')?.value
+        })
+        .pipe(take(1))
+        .subscribe({
+          next: (x) => {
+            this._router.navigate(['/leads']);
+          }
+        });
+    }
+  }
 }
