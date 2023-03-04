090b466a4cc7bc75a62f08f526e742defc3cca02
Maciej Paluch <maciekpal@gmail.com>
2023-03-04 15:21:25 +0100

    Feat: bio information form created and displayed

--- src/app/components/bio/bio.component-module.ts
+++ src/app/components/bio/bio.component-module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BioComponent } from './bio.component';
 
 @NgModule({
-  imports: [],
+  imports: [ReactiveFormsModule],
   declarations: [BioComponent],
   providers: [],
   exports: [BioComponent]
--- src/app/components/bio/bio.component.ts
+++ src/app/components/bio/bio.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-bio',
@@ -7,4 +8,7 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BioComponent {
+  readonly bioForm: FormGroup = new FormGroup({ bioText: new FormControl() });
+
+  onBioFormSubmitted(bioForm: FormGroup): void {}
 }
