b1452cbb91d81bea9120082940e73c5b8bc8e5a3
Maciej Paluch <maciekpal@gmail.com>
2023-03-04 16:54:39 +0100

    Feat: bio form functionality and vlaidators added

--- src/app/components/bio/bio.component.ts
+++ src/app/components/bio/bio.component.ts
@@ -1,8 +1,24 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { Router } from '@angular/router';
 import { UserModel } from '../../models/user.model';
 import { UserService } from '../../services/user.service';
+
+export const minTenWords: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const bioText = control.value as string;
+  if (bioText) {
+    return bioText.match(/(\s\S+){9,}/) ? null : { minTenWords: true };
+  } else return null;
+};
+
+export const minTwoSentences: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const bioText = control.value as string;
+  if (bioText) {
+    return bioText.match(/.+[.!?].+\w+/) ? null : { minTwoSentences: true };
+  } else return null;
+};
 
 @Component({
   selector: 'app-bio',
@@ -11,10 +27,21 @@ import { UserService } from '../../services/user.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BioComponent {
-  readonly bioForm: FormGroup = new FormGroup({ bioText: new FormControl() });
+  readonly bioForm: FormGroup = new FormGroup({
+    bioText: new FormControl('', [Validators.required, minTenWords, minTwoSentences])
+  });
   readonly userDetails$: Observable<UserModel> = this._userService.me();
 
-  constructor(private _userService: UserService) {}
+  constructor(private _userService: UserService, private _router: Router) {}
 
-  onBioFormSubmitted(bioForm: FormGroup): void {}
+  onBioFormSubmitted(bioForm: FormGroup): void {
+    if (bioForm.valid) {
+      this._userService
+        .postUserBio(bioForm.get('bioText')?.value)
+        .pipe(take(1))
+        .subscribe({
+          next: (x) => this._router.navigate(['/leads'])
+        });
+    }
+  }
 }
