80096e6a224df14d7c1a9468446eb82dc7ac6e45
Maciej Paluch <maciekpal@gmail.com>
2023-03-04 11:12:25 +0100

    Feat: register comp added to route /auth/register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LeadsComponent } from './components/leads/leads.component';
+import { LeadsComponentModule } from './components/leads/leads.component-module';
+import { RegisterComponent } from './components/register/register.component';
+import { RegisterComponentModule } from './components/register/register.component-module';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignInComponentModule } from './components/sign-in/sign-in.component-module';
 
@@ -11,11 +14,12 @@ const routes: Routes = [
     redirectTo: '/auth/login',
     pathMatch: 'full'
   },
+  { path: 'auth/register', component: RegisterComponent },
   { path: 'leads', component: LeadsComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), SignInComponentModule],
+  imports: [RouterModule.forRoot(routes), SignInComponentModule, RegisterComponentModule, LeadsComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
