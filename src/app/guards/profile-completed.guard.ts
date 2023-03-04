572b9f415986be3bbdf828f989fd6e588ac09121
Maciej Paluch <maciekpal@gmail.com>
2023-03-04 17:10:48 +0100

    Feat: guard for uncompleted profile created

---
+++ src/app/guards/profile-completed.guard.ts
@@ -0,0 +1,24 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { catchError, map, Observable, of, throwError } from 'rxjs';
+import { UserService } from '../services/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileCompletedGuard implements CanActivate {
+  constructor(private _userService: UserService, private _router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
+    return this._userService.checkUserBio().pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 404) {
+          return of('notCompleted');
+        }
+        return throwError(() => error);
+      }),
+      map((response) => (response === 'notCompleted' ? this._router.parseUrl('/complete-profile') : true))
+    );
+  }
+}
