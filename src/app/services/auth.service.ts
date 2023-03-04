4256ce8b3e55ea676751b0204e62a2186acd649d
Maciej Paluch <maciekpal@gmail.com>
2023-03-04 11:01:34 +0100

    Feat: storing tokens in localStorage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,15 +8,19 @@ import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private _accessTokenSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
-  public readonly accessToken$: Observable<string> = this._accessTokenSubject.asObservable();
+  private _accessTokenSubject: BehaviorSubject<string | null> = new BehaviorSubject<string | null>(
+    this._storage.getItem('accessToken')
+  );
+  public readonly accessToken$: Observable<string | null> = this._accessTokenSubject.asObservable();
 
-  private _refreshTokenSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
-  public readonly refreshToken$: Observable<string> = this._refreshTokenSubject.asObservable();
+  private _refreshTokenSubject: BehaviorSubject<string | null> = new BehaviorSubject<string | null>(
+    this._storage.getItem('refreshToken')
+  );
+  public readonly refreshToken$: Observable<string | null> = this._refreshTokenSubject.asObservable();
 
-  constructor(private _httpClient: HttpClient) {}
+  constructor(private _httpClient: HttpClient, private _storage: Storage) {}
 
-  public login(login: { email: string; password: string }): Observable<UserCredentialsModel> {
+  public login(login: { email: string; password: string }, rememberMe: boolean): Observable<UserCredentialsModel> {
     return this._httpClient
       .post<AuthLoginResponse<UserCredentialsModel>>(`${environment.apiUrl}/auth/login`, {
         data: login
@@ -26,12 +30,16 @@ export class AuthService {
           accessToken: response.data.accessToken,
           refreshToken: response.data.refreshToken
         })),
-        tap((tokens) => this.logInUser(tokens))
+        tap((tokens) => this.logInUser(tokens, rememberMe))
       );
   }
 
-  private logInUser(tokens: UserCredentialsModel): void {
+  private logInUser(tokens: UserCredentialsModel, rememberMe: boolean): void {
     this._accessTokenSubject.next(tokens.accessToken);
     this._refreshTokenSubject.next(tokens.refreshToken);
+    if (rememberMe) {
+      this._storage.setItem('accessToken', tokens.accessToken);
+      this._storage.setItem('refreshToken', tokens.refreshToken);
+    }
   }
 }
