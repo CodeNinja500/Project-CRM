35b17212de1a2ab23e9bb2f49972a37505a6a18d
Maciej Paluch <maciekpal@gmail.com>
2023-03-04 10:51:25 +0100

    Local Storage provided

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  providers: [{ provide: Storage, useValue: localStorage }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
