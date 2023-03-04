624092b3141491564dee01cf40163e7cb4c2a1f9
Maciej Paluch <maciekpal@gmail.com>
2023-03-04 20:40:56 +0100

    debug: auto login guard console log for debugging

--- src/app/guards/auto-login.guard.ts
+++ src/app/guards/auto-login.guard.ts
@@ -10,6 +10,7 @@ export class AutoLoginGuard implements CanActivate {
   constructor(private _authService: AuthService, private _router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
+    console.log('auto login guard activated');
     return this._authService.accessToken$.pipe(map((token) => (token ? this._router.parseUrl('/leads') : true)));
   }
 }
