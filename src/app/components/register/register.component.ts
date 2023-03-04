9b9eb2c488795e4e3afcbb15517daba7aba308c5
Maciej Paluch <maciekpal@gmail.com>
2023-03-04 11:53:08 +0100

    Feat: Validators added to registerForm

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,39 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
+export const hasSmallLetter: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const password = control.value as string;
+  if (password.match(/[a-z]+/)) return null;
+  else return { hasSmallLetter: true };
+};
+
+export const hasCapitalLetter: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const password = control.value as string;
+  if (password.match(/[A-Z]+/)) return null;
+  else return { hasCapitalLetter: true };
+};
+
+export const hasSpecialSign: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const password = control.value as string;
+  if (password.match(/[^a-zA-Z]+/)) return null;
+  else return { hasSpecialSign: true };
+};
+
+export const hasNumber: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const password = control.value as string;
+  if (password.match(/[0-9]+/)) return null;
+  else return { hasNumber: true };
+};
+
+export const passwordsMatch: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const password = control.get('password')?.value as string;
+  const repeatPassword = control.get('repeatPassword')?.value as string;
+
+  if (password && repeatPassword) {
+    password === repeatPassword ? null : { passwordsMatch: true };
+  }
+  return null;
+};
 
 @Component({
   selector: 'app-register',
@@ -8,11 +42,21 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RegisterComponent {
-  readonly registerForm: FormGroup = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required]),
-    repeatPassword: new FormControl('', [Validators.required]),
-    termsPolicy: new FormControl(false, [Validators.requiredTrue])
-  });
+  readonly registerForm: FormGroup = new FormGroup(
+    {
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        hasSmallLetter,
+        hasCapitalLetter,
+        hasSpecialSign,
+        hasNumber
+      ]),
+      repeatPassword: new FormControl('', [Validators.required]),
+      termsPolicy: new FormControl(false, [Validators.requiredTrue])
+    },
+    { validators: passwordsMatch }
+  );
 }
