50a236ec7b6edfd4e8d46d35250f356dcc66ce24
Maciej Paluch <maciekpal@gmail.com>
2023-03-05 18:34:55 +0100

    Fix: auto login guard fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
   {
     path: 'auth/login',
     component: SignInComponent,
-    data: { redirectAutoLogin: '/auth/login' },
+    data: { redirectAutoLogin: '/leads' },
     canActivate: [AutoLoginGuard]
   },
   {
