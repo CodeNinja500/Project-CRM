8bc045be211214d2ac7a7276d6717277e6ffa6db
Maciej Paluch <maciekpal@gmail.com>
2023-03-05 18:35:16 +0100

    Fix: added take(1) in guards

--- src/app/guards/profile-completed.guard.ts
+++ src/app/guards/profile-completed.guard.ts
@@ -1,7 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { catchError, map, Observable, of, throwError } from 'rxjs';
+import { catchError, map, Observable, of, take, throwError } from 'rxjs';
 import { UserService } from '../services/user.service';
 
 @Injectable({
@@ -13,6 +13,7 @@ export class ProfileCompletedGuard implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
     console.log('complete profile activated');
     return this._userService.checkUserBio().pipe(
+      take(1),
       catchError((error: HttpErrorResponse) => {
         if (error.status === 404) {
           return of('notCompleted');
