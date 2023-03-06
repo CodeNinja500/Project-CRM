b72e0fcd62e29572bdd7ef2f5db1881c8219156b
Maciej Paluch <maciekpal@gmail.com>
2023-03-06 17:02:32 +0100

    Feat: create lead component added to routing

--- src/app/routes/leads.routes.ts
+++ src/app/routes/leads.routes.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LeadCreateComponent } from '../components/lead-create/lead-create.component';
+import { LeadCreateComponentModule } from '../components/lead-create/lead-create.component-module';
 import { LeadListComponent } from '../components/lead-list/lead-list.component';
 import { LeadListComponentModule } from '../components/lead-list/lead-list.component-module';
 
@@ -7,9 +9,11 @@ import { LeadListComponentModule } from '../components/lead-list/lead-list.compo
   imports: [
     RouterModule.forChild([
       { path: 'leads', component: LeadListComponent },
+      { path: 'create-lead', component: LeadCreateComponent },
       { path: '', redirectTo: '/leads', pathMatch: 'full' }
     ]),
-    LeadListComponentModule
+    LeadListComponentModule,
+    LeadCreateComponentModule
   ],
   exports: []
 })
