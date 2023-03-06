7e6694dc50a696707c7d5e997c64f243bc83177e
Maciej Paluch <maciekpal@gmail.com>
2023-03-06 16:37:53 +0100

    Feat: button create lead only for admins

--- src/app/components/lead-list/lead-list.component.ts
+++ src/app/components/lead-list/lead-list.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
 import { Observable } from 'rxjs';
 import { LeadModel } from '../../models/lead.model';
 import { LeadsService } from '../../services/leads.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-lead-list',
@@ -11,6 +12,7 @@ import { LeadsService } from '../../services/leads.service';
 })
 export class LeadListComponent {
   readonly leadList$: Observable<LeadModel[]> = this._leadsService.getAll();
+  readonly isAdmin$: Observable<boolean> = this._userService.isAdmin();
 
-  constructor(private _leadsService: LeadsService) {}
+  constructor(private _leadsService: LeadsService, private _userService: UserService) {}
 }
