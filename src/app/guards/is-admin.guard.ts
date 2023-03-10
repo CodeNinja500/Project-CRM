95de5dad4ca9fd0675b9d252706e168e2991e2a8
Maciej Paluch <maciekpal@gmail.com>
2023-03-10 14:45:10 +0100

    fix: take(1) added

--- src/app/guards/is-admin.guard.ts
+++ src/app/guards/is-admin.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { map, Observable } from 'rxjs';
+import { map, Observable, take } from 'rxjs';
 import { UserService } from '../services/user.service';
 
 @Injectable({
@@ -10,8 +10,9 @@ export class IsAdminGuard implements CanActivate {
   constructor(private _userService: UserService, private _router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
-    return this._userService
-      .isAdmin()
-      .pipe(map((isAdmin) => (isAdmin ? true : this._router.parseUrl(route.data['redirectNotAdmin'] || '/leads'))));
+    return this._userService.isAdmin().pipe(
+      take(1),
+      map((isAdmin) => (isAdmin ? true : this._router.parseUrl(route.data['redirectNotAdmin'] || '/leads')))
+    );
   }
 }
