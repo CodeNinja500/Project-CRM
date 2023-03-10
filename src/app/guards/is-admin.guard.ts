4e523e68282d49883bd1421f6a7ef0e010e25897
Maciej Paluch <maciekpal@gmail.com>
2023-03-10 14:31:18 +0100

    Feat: isAdmin guard finished and added to route

--- src/app/guards/is-admin.guard.ts
+++ src/app/guards/is-admin.guard.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IsAdminGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(private _userService: UserService, private _router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
+    return this._userService.isAdmin().pipe(map((isAdmin) => (isAdmin ? true : this._router.parseUrl('/leads'))));
   }
-  
 }
